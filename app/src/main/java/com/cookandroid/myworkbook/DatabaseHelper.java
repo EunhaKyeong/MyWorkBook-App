d29e89e5fbf8b297f671174c655a196e71fd513d
EunhaKyeong <eh0634a@gmail.com>
2021-06-20 01:56:08 +0900

    db에서 nickname 가져오는 쿼리문 작성

--- app/src/main/java/com/cookandroid/myworkbook/DatabaseHelper.java
+++ app/src/main/java/com/cookandroid/myworkbook/DatabaseHelper.java
@@ -9,7 +9,7 @@ import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "myWorkBookDB.db";
-    private SQLiteDatabase sqlDB;
+    private SQLiteDatabase sqlDB = this.getWritableDatabase();;
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -26,15 +26,23 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
     //user 정보 select
     public Cursor getUser(String id, String pwd) {
-        sqlDB = this.getWritableDatabase();
         String query = "SELECT * FROM user WHERE userID='" + id + "' AND password='" + pwd + "';";
         Cursor cursor = sqlDB.rawQuery(query, null);
 
         return cursor;
     }
+
+    //SELECT nickname
+    public String getNickname(int userPK) {
+        String query = "SELECT nickname FROM user WHERE userPK=?";
+        Cursor cursor = sqlDB.rawQuery(query, new String[] {String.valueOf(userPK)});
+        cursor.moveToFirst();
+
+        return cursor.getString(cursor.getColumnIndex("nickname"));
+    }
+
     //exam 정보 select
     public Cursor getExams(int userPK) {
-        sqlDB = this.getReadableDatabase();
         String query = "SELECT examTitle, examPK FROM exam WHERE userPK=" + userPK + ";";
         Cursor cursor = sqlDB.rawQuery(query, null);
 
